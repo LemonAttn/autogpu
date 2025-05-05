82e47d5d566a36df46d532eb28919101f3db9d94
LemonAttn <1837669410@qq.com>
2025-05-05 21:19:38 +0800

    增加查询钱包的示例

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from autogpu import gpu, watch, login
 import config
 
 # 登陆
-login.login()
+# login.login()
 
 
 # 监控gpu
-gpu_info = watch.watch_gpu(config = config, gpu = 'RTX 4090')
-print(gpu_info)
+# gpu_info = watch.watch_gpu(config = config, gpu = 'RTX 4090')
+# print(gpu_info)
 
 
 # 购买gpu
-gpu.use(config = config, gpu = 'RTX 4090')
+gpu.use(config = config, gpu = 'RTX 3090')
+
+# 钱包信息
+# wallet = watch.watch_wallet(config)
+# print(wallet)
