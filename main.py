91d2b7b6739cf44e58fbfb3612f3fa2f69bfb002
LemonAttn <1837669410@qq.com>
2025-05-05 12:20:16 +0800

    增加登陆

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from autogpu import gpu, watch
+from autogpu import gpu, watch, login
 import config
+
+# 登陆
+login.login()
+
 
 # 监控gpu
 gpu_info = watch.watch_gpu(config = config, gpu = 'RTX 4090')
 print(gpu_info)
+
 
 # 购买gpu
 gpu.use(config = config, gpu = 'RTX 4090')
